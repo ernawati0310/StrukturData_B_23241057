86e971c3a5e23ac190e16ad0ae1959e077314a2a
ernawati0310 <146051500+ernawati0310@users.noreply.github.com>
2024-06-03 10:54:15 +0700

    Add files via upload

---
+++ STRUKTUR DATA/meet9.py
@@ -0,0 +1,22 @@
+# Percabangan jika maka
+
+name = input("what is your name : ")
+#if kondisi : aksi
+
+#if standar
+if name == "Aulia" : print("Mahasiswa undikma")
+print("end of program")
+
+# 2. if indentasi
+if name=="aulia":
+    print("mahasiswa undikma")
+    print("kelas pti b")
+print("end of program")
+
+# 3. IF ELSE
+if name=="aulia":
+    print("mahasiswa undikma")
+    print("terimakasih")
+else:
+    print("anda bukan mahasiswa")
+print("end of program")
