b7732cb2718bdbfb476502087c57f1ac44d71f1a
ernawati0310 <146051500+ernawati0310@users.noreply.github.com>
2024-06-10 11:32:09 +0700

    Add files via upload

---
+++ STRUKTUR DATA/meet10.py
@@ -0,0 +1,13 @@
+# belajar perbandingan lanjutan, ELIF
+
+nama = input("masukkan nama :")
+
+if nama=="aulia": # kondisi 1
+    print("mahasiswi undikma") # aksi true 1
+elif nama=="nisa": # kondisi 2
+    print("kelas pti ") # aksi true 2
+elif nama=="erna": # kondisi 3
+    print("bukan mahasiswi") # aksi true 3
+else:
+    print("bukan kelas pti") # aksi False
+print("program selesai")
