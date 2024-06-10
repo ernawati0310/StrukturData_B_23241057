6f7f92c172eb88b9f53c46f2b61aa4cdde0cda31
ernawati0310 <146051500+ernawati0310@users.noreply.github.com>
2024-06-10 11:32:34 +0700

    Add files via upload

---
+++ STRUKTUR DATA/app penjumlahan dan pengurangan.py
@@ -0,0 +1,15 @@
+# Aplikasi Penjumlahan Sederhana
+
+# Meminta input dari pengguna
+num1 = input("Masukkan angka pertama: ")
+num2 = input("Masukkan angka kedua: ")
+
+# Mengubah input menjadi bilangan bulat
+num1 = int(num1)
+num2 = int(num2)
+
+# Menjumlahkan dua bilangan
+result = num1 + num2
+
+# Menampilkan hasil penjumlahan
+print("Hasil penjumlahan:", result)
