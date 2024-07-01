b96428836dedb5c3924b54f14eef64df60f5bbf9
ernawati0310 <146051500+ernawati0310@users.noreply.github.com>
2024-07-01 08:55:05 +0700

    UAS

--- STRUKTUR DATA/meet3.py
+++ STRUKTUR DATA/meet3.py
@@ -1,21 +1,13 @@
-# contoh tipe data (integer)  : angka satuan bulat
-bil_integer = 1
-print(type(bil_integer))
-print ("type data dari bil_integer adalah: ",type(bil_integer))
- 
- # contoh tipe data (float) : bilangan desimal
-bil_float = 2.5
-print ("type data bil_float adalah: ", type(bil_float))
+angka = 15
 
-# contoh tipe data (string) :kumpulan karakter
-bil_string = "putu"
-print ("type data bil_string adalah: ", type(bil_string))
+# Percabangan
+if angka > 15:
+    print("Angka lebih besar dari 15")
+elif angka < 15:
+    print("Angka lebih kecil dari 15")
+else:
+    print("Angka sama dengan 15")
 
-# contoh tipe data (bolean) : biner, 0,1, True False
-bil_bol = True
-print ("type data bil_bol adalah: ", type(bil_bol))
-
-# tipe data dari bahasa C
-from ctypes import c_double
-bil_double  = c_double((20000000))
-print ("type data bi_double adalah:", type(bil_double))
+# Perulangan
+for i in range(12):
+    print("Ini adalah perulangan ke-", i+1)
